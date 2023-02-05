374ec52bc8a704e5f9f85198a6a89cea88bf0c5d
supernatychacz <supernatychacz@gmail.com>
2023-02-05 15:05:55 +0000

    new styles

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var i = 0;
-var txt = document.getElementById("txt");
-var speed = 50;
-
-function typeWriter() {
-  if (i < txt.lenght) {
-    document.getElementById("txt").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+const text = document.getElementById("txt");
+const str = text.textContent + "Ewa Malik";
+let index = 0;
+const typeWriter = () => {
+  if (index < str.length) {
+    text.textContent += str.charAt(index);
+    index++;
+    setTimeout(typeWriter, 300);
   }
-}
+};
+typeWriter();
+
+const emailBtn = document.getElementById("emailBtn");
+emailBtn.addEventListener("click", function () {
+  window.location.href = "mailto:supernatychacz@gmail.com";
+});
